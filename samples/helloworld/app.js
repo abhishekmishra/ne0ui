39dbd1b314523f0068d68136342f0fc4b557bbd9
Abhishek Mishra <abhishekmishra3@gmail.com>
2022-04-03 12:03:25 +0530

    comments

--- samples/helloworld/app.js
+++ samples/helloworld/app.js
@@ -1,9 +1,14 @@
 import { NuTop, NuSingleLineText, NuSizeHint } from "../../index.mjs";
 
+// create the toplevel window
 var top = new NuTop();
 
+// create and add a single line text control to the toplevel
 var text = new NuSingleLineText(200, '50', 'HELLO WORLD!');
 top.appendRect(text);
 
+// center the text control in the parent window
 text.centerParent();
+
+// center justify the text in the text control
 text.justify('center');
