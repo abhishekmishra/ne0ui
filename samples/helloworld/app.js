9ca8abdb2070aab7968f8b7d2664c45471486865
Abhishek Mishra <abhishekmishra3@gmail.com>
2022-04-03 12:31:56 +0530

    added event dispatch and handling

--- samples/helloworld/app.js
+++ samples/helloworld/app.js
@@ -7,13 +7,12 @@ var top = new NuTop();
 var text = new NuSingleLineText(200, '50', 'HELLO WORLD!');
 top.appendRect(text);
 
-// center the text control in the parent window
-text.centerParent();
-
 // center justify the text in the text control
 text.justify('center');
 
-// center the text element when parent is resized
+// center the text element in parent
+// and keep centered when parent is resized
+text.centerParent();
 text.getParent().onEvent('nu_resize', (evt) => {
     text.centerParent();
 });
