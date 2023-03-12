106ea37e97e65242ad5caa3539ba9bcb8ddd5ab2
Abhishek Mishra <abhishekmishra3@gmail.com>
2023-03-12 09:03:32 +0530

    api docs

--- src/iframe.mjs
+++ src/iframe.mjs
@@ -1,9 +1,19 @@
-import { NuUIComponent } from "./uicomp.mjs";
+import { NuUICompConfig, NuUIComponent } from "./uicomp.mjs";
 
 /**
  * Create a standard html button, with text and icon both optional
  */
 export class NuIFrame extends NuUIComponent {
+
+    /**
+     * Create a new iframe based ui component which can host webpage.
+     * 
+     * the configuration should contain a key 'src', whose value will
+     * become the iframe src attribute. In an iframe the src points to the
+     * webpage which will be displayed inside the iframe.
+     * 
+     * @param {NuUICompConfig} config configuration for the iframe component
+     */
     constructor(config) {
         super(document.createElement('iframe'), config);
     }
@@ -18,11 +28,13 @@ export class NuIFrame extends NuUIComponent {
         super.applyConfig();
 
         const src = this.getCfg('src');
-        const text = this.getCfg('text');
-
         this.elem.setAttribute('src', src);
     }
 
+    /**
+     * Set the iframe "src"
+     * @param {string} src url of the webpage to embed in the iframe
+     */
     setSrc(src) {
         this.setCfg('src', src);
     }
